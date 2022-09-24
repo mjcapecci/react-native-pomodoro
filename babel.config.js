db3dd78038ba1936ac4ad44d462806799a0336d2
Mike Capecci <mjcapecci@gmail.com>
2022-09-24 16:34:51 -0500

    test: finishes implementing new testing infrastructure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
