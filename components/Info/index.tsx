58f796bac88866291255f2400722e7a6d3b23a52
Mike Capecci <mjcapecci@gmail.com>
2023-01-29 10:27:10 -0600

    feat(dangerzone): adds danger zone modal initial implementation

--- components/Info/index.tsx
+++ components/Info/index.tsx
@@ -4,9 +4,12 @@ import { ColorType } from '../../types'
 import HighLevelDescription from './HighLevelDescription'
 import IconKey, { IconKeyData } from './IconKey'
 import { AppContext } from '../General/AppContextProvider'
+import DangerZoneButton from './DangerZoneButton'
+import DangerModal from './DangerModal'
 
 const Help = (): JSX.Element => {
   const { appVersion } = React.useContext(AppContext)
+  const [dangerModalVisible, setDangerModalVisible] = React.useState(false)
 
   const baseIconTableData: IconKeyData[] = [
     { icon: 'hammer', description: 'Work Round -- 25 Minutes.' },
@@ -34,16 +37,18 @@ const Help = (): JSX.Element => {
     <ScrollView>
       <View style={styles.container}>
         <HighLevelDescription
-          description='[App Name] is a minimalist app that helps you to implement the Pomodoro method into your life
+          description='Pomodash is a minimalist app that helps you to implement the Pomodoro method into your life
       with very little setup.'
         />
         <IconKey tableTitle='Base Icon Key' tableData={baseIconTableData} />
         <IconKey tableTitle='Color Key' tableData={colorKeyTableData} />
-        <HighLevelDescription description='Navigation through [App Name] is simple and is completed through the use of the bottom navigation bar.' />
+        <HighLevelDescription description='Navigation through Pomodash is simple and is completed through the use of the bottom navigation bar.' />
         <IconKey tableTitle='Navigation Icon Key' tableData={navigationIconTableData} />
+        <DangerZoneButton setDangerModalVisible={setDangerModalVisible} />
         <HighLevelDescription
           description={`The current installed app version is: ${appVersion} -- Some updates are installed automatically, but please check for updates regularly.`}
         />
+        <DangerModal showModal={dangerModalVisible} setDangerModalVisible={setDangerModalVisible} />
       </View>
     </ScrollView>
   )
@@ -55,6 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-evenly',
     backgroundColor: '#000',
+    marginBottom: 15,
   },
   text: {
     color: '#fff',
